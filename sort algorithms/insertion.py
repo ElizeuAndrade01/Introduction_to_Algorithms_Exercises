f39f45650dab18d4889728ca48c36146f1759c7d
ElizeuAndrade01 <elizeu.feitosa@hotmail.com>
2025-12-12 22:33:08 -0300

    Modulo 1: Fundamentos e Análise Assintótica [Insertion e Merge]

---
+++ sort algorithms/insertion.py
@@ -0,0 +1,10 @@
+#insertion code pseudocode
+#
+#for j = 2 para Aconjunto
+#   chave = A[j]
+#   //inserir na sequência ordenada A[1, ...j-1]
+#   i = j-1
+#   while i>0 e A[i]>chave
+#       A[i+1] = A[i]
+#       i = i-1
+#   A[i+1] = chave
